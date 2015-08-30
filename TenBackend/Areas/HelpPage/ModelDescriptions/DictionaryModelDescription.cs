4b73a742d11b4dfd5eecb011f654f92cd91f02e8
Yumen Cao <gawainus97@gmail.com>
2015-08-30 12:56:18 -0500

    init

---
+++ TenBackend/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TenBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TenBackend/Controllers/ApplicationUserController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TenBackend.Models;
+
+namespace TenBackend.Controllers
+{
+    public class ApplicationUserController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ApplicationUser
+        public IQueryable<ApplicationUser> GetApplicationUsers()
+        {
+            return db.ApplicationUsers;
+        }
+
+        // GET: api/ApplicationUser/5
+        [ResponseType(typeof(ApplicationUser))]
+        public async Task<IHttpActionResult> GetApplicationUser(string id)
+        {
+            ApplicationUser applicationUser = await db.ApplicationUsers.FindAsync(id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(applicationUser);
+        }
+
+        // PUT: api/ApplicationUser/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutApplicationUser(string id, ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != applicationUser.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(applicationUser).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApplicationUser
+        [ResponseType(typeof(ApplicationUser))]
+        public async Task<IHttpActionResult> PostApplicationUser(ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ApplicationUsers.Add(applicationUser);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ApplicationUserExists(applicationUser.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = applicationUser.Id }, applicationUser);
+        }
+
+        // DELETE: api/ApplicationUser/5
+        [ResponseType(typeof(ApplicationUser))]
+        public async Task<IHttpActionResult> DeleteApplicationUser(string id)
+        {
+            ApplicationUser applicationUser = await db.ApplicationUsers.FindAsync(id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            db.ApplicationUsers.Remove(applicationUser);
+            await db.SaveChangesAsync();
+
+            return Ok(applicationUser);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return db.ApplicationUsers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
